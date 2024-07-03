c474533ee6f971238577a8609abb418eb33146d4
Nitin Khare <kharenitin777@gmail.com>
2024-07-03 16:16:59 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
                     initial="hidden"
                     animate="visible"
                     className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>
-                        Nitin Khar
+                        Nitin Khare
                     </motion.h1>
                     <motion.span 
                     variants={container(1)}
