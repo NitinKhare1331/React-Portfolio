0dd7f9751bac2b7be8c63fe9e644b179565d1bde
Nitin Khare <kharenitin777@gmail.com>
2024-07-09 11:50:20 +0530

    underline hover animation added to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import './About.css'
 
 const Navbar = () => {
 
@@ -9,10 +10,10 @@ const Navbar = () => {
             Portfolio
         </div>
         <div className='m-8 flex items-center justify-center gap-8 text-xl'>
-            <a href='#about' className='hidden lg:block'>About Me</a>
-            <a href='#technologies' className='hidden lg:block'>Technologies</a>
-            <a href='#projects' className='hidden lg:block'>Projects</a>
-            <a href='#contact' className='hidden lg:block'>Contact</a>
+            <a href='#about' id='about_me' className='hidden lg:block'>About Me</a>
+            <a href='#technologies' id='tech' className='hidden lg:block'>Technologies</a>
+            <a href='#projects' id='project' className='hidden lg:block'>Projects</a>
+            <a href='#contact' id='contact' className='hidden lg:block'>Contact</a>
             <a href='https://linkedin.com/in/13-nitin-khare' target='_blank'><FaLinkedin /></a>
             <a href='https://github.com/NitinKhare1331' target='_blank'><FaGithub /></a>
         </div>
